7f14f1f33dd91905319edd61979220a2bb97e133
Mir <meermohammed80@gmail.com>
2026-03-31 20:37:02 +0300

    fix push

---
+++ src/config/google_oauth.local.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'client_id' => '1040337035348-uae013taiu1mor7jk0fv8v393edqsq2t.apps.googleusercontent.com',
+    'client_secret' => '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',
+    'redirect_uri' => 'http://localhost/Flower-Store/google_callback.php'
+];
